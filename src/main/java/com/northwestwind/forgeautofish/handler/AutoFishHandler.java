fe6d31c9ba16e6965ba09534e03d841deba1f869
NorthWestWind <wsyn148@gmail.com>
2020-08-03 10:31:27 +0800

    AutoFish for 1.13.2

--- src/main/java/com/northwestwind/forgeautofish/handler/AutoFishHandler.java
+++ src/main/java/com/northwestwind/forgeautofish/handler/AutoFishHandler.java
@@ -3,18 +3,18 @@ package com.northwestwind.forgeautofish.handler;
 import com.northwestwind.forgeautofish.AutoFish;
 import com.northwestwind.forgeautofish.keybind.KeyBinds;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.network.play.ClientPlayNetHandler;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FishingRodItem;
-import net.minecraft.network.play.client.CPlayerTryUseItemPacket;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityFishHook;
+import net.minecraft.item.ItemFishingRod;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -28,7 +28,7 @@ public class AutoFishHandler {
     public void onKeyInput(InputEvent.KeyInputEvent e) {
         if(KeyBinds.autofish.isPressed()) {
             autofishenabled = !autofishenabled;
-            Minecraft.getInstance().player.sendStatusMessage(new TranslationTextComponent("toggle.forgeautofish", (autofishenabled ? "\u00A7aEnabled" : "\u00A7cDisabled")), true);
+            Minecraft.getInstance().player.sendStatusMessage(new TextComponentTranslation("toggle.forgeautofish", (autofishenabled ? "\u00A7aEnabled" : "\u00A7cDisabled")), true);
         }
     }
 
@@ -38,23 +38,23 @@ public class AutoFishHandler {
     @OnlyIn(Dist.CLIENT)
     public void onPlayerTick(TickEvent.PlayerTickEvent e) throws InterruptedException {
         if(!autofishenabled) return;
-        if(!(e.player.getHeldItemMainhand().getItem() instanceof FishingRodItem)) {
+        if(!(e.player.getHeldItemMainhand().getItem() instanceof ItemFishingRod)) {
             return;
         }
-        if(e.player.fishingBobber == null) {
+        if(e.player.fishEntity == null) {
             return;
         }
         if(fished) return;
-        Vector3d vector = e.player.fishingBobber.getMotion();
-        double x = vector.getX();
-        double z = vector.getZ();
-        double y = vector.getY();
-        if(y < -0.1 && e.player.fishingBobber.isInWater() && x == 0 && z == 0) {
-            ClientPlayNetHandler nethandler = Minecraft.getInstance().getConnection();
+        EntityFishHook fishingHook = e.player.fishEntity;
+        double x = fishingHook.motionX;
+        double z = fishingHook.motionZ;
+        double y =fishingHook.motionY;
+        if(y < -0.1 && e.player.fishEntity.isInWater() && x == 0 && z == 0) {
+            NetHandlerPlayClient nethandler = Minecraft.getInstance().getConnection();
             if(nethandler != null) {
-                nethandler.sendPacket(new CPlayerTryUseItemPacket(Hand.MAIN_HAND));
+                nethandler.sendPacket(new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND));
             } else {
-                rightClick(e.player.world, e.player, Hand.MAIN_HAND);
+                rightClick(e.player.world, e.player, EnumHand.MAIN_HAND);
             }
             if(!fished) {
                 fished = true;
@@ -67,9 +67,9 @@ public class AutoFishHandler {
                     try {
                         Thread.sleep(500);
                         if(nethandler != null) {
-                            nethandler.sendPacket(new CPlayerTryUseItemPacket(Hand.MAIN_HAND));
+                            nethandler.sendPacket(new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND));
                         } else {
-                            rightClick(e.player.world, e.player, Hand.MAIN_HAND);
+                            rightClick(e.player.world, e.player, EnumHand.MAIN_HAND);
                         }
                     } catch (InterruptedException ex) {
                         ex.printStackTrace();
@@ -93,7 +93,7 @@ public class AutoFishHandler {
         }).start();
     }
 
-    private void rightClick(World world, PlayerEntity player, Hand hand) {
+    private void rightClick(World world, EntityPlayer player, EnumHand hand) {
         player.getHeldItemMainhand().useItemRightClick(world, player, hand);
     }
 }
--- src/main/java/com/northwestwind/forgeautofish/keybind/KeyBinds.java
+++ src/main/java/com/northwestwind/forgeautofish/keybind/KeyBinds.java
@@ -1,7 +1,7 @@
 package com.northwestwind.forgeautofish.keybind;
 
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public class KeyBinds {
@@ -9,7 +9,7 @@ public class KeyBinds {
     public static KeyBinding autofish;
 
     public static void register() {
-        autofish = new KeyBinding(new TranslationTextComponent("key.forgeautofish.autofish").getString(), 43, "key.categories.forgeautofish");
+        autofish = new KeyBinding(new TextComponentTranslation("key.forgeautofish.autofish").getString(), 43, "key.categories.forgeautofish");
 
         ClientRegistry.registerKeyBinding(autofish);
     }
