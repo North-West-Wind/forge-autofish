d2b52c9475a0179c2595bd0df3e000466b863998
NorthWestWind <wsyn148@gmail.com>
2020-08-08 13:46:04 +0800

    Released 1.9.4

--- src/main/java/com/northwestwind/forgeautofish/AutoFish.java
+++ src/main/java/com/northwestwind/forgeautofish/AutoFish.java
@@ -15,8 +15,7 @@ public class AutoFish
 {
     public static final String MODID = "forgeautofish";
     public static final String NAME = "AutoFish for Forge";
-    public static final String VERSION = "1.0.3";
-    public static final String ACCEPTED = "[1.12.2]";
+    public static final String VERSION = "1.0.4";
     public static final String CLIENT_PROXY = "com.northwestwind.forgeautofish.proxy.ClientProxy";
     public static final String COMMON_PROXY = "com.northwestwind.forgeautofish.proxy.CommonProxy";
 
